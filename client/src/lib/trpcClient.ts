85f58e0687bf68c4156ced88f4b74b82feaf082c
rodrigogpx <rodrigogpx@gmail.com>
2026-03-07 12:38:40 -0300

    fix: use splitLink to route mutations via httpLink (POST) fixing 405 METHOD_NOT_SUPPORTED error

--- client/src/lib/trpcClient.ts
+++ client/src/lib/trpcClient.ts
@@ -1,32 +1,45 @@
 import { QueryClient } from '@tanstack/react-query';
-import { httpBatchLink } from '@trpc/client';
+import { httpBatchLink, httpLink, splitLink } from '@trpc/client';
 import superjson from 'superjson';
 import { trpc } from './trpc';
 import { extractTenantSlugFromPath } from '@/_core/hooks/useTenantSlug';
 
 export const queryClient = new QueryClient();
 
+function getTrpcHeaders() {
+  const slug = typeof window !== 'undefined'
+    ? extractTenantSlugFromPath(window.location.pathname)
+    : null;
+
+  return {
+    'x-trpc-source': 'client',
+    ...(slug ? { 'x-tenant-slug': slug } : {}),
+  };
+}
+
+function trpcFetch(url: URL | RequestInfo, options?: RequestInit) {
+  return fetch(url, {
+    ...options,
+    credentials: 'include',
+  });
+}
+
 export const trpcClient = trpc.createClient({
   links: [
-    httpBatchLink({
-      url: '/api/trpc',
-      transformer: superjson,
-      headers() {
-        const slug = typeof window !== 'undefined'
-          ? extractTenantSlugFromPath(window.location.pathname)
-          : null;
-
-        return {
-          'x-trpc-source': 'client',
-          ...(slug ? { 'x-tenant-slug': slug } : {}),
-        };
-      },
-      fetch(url, options) {
-        return fetch(url, {
-          ...options,
-          credentials: 'include',
-        });
-      },
+    splitLink({
+      condition: (op) => op.type === 'mutation',
+      true: httpLink({
+        url: '/api/trpc',
+        transformer: superjson,
+        headers: getTrpcHeaders,
+        fetch: trpcFetch,
+      }),
+      false: httpBatchLink({
+        url: '/api/trpc',
+        transformer: superjson,
+        headers: getTrpcHeaders,
+        fetch: trpcFetch,
+      }),
     }),
   ],
 });
